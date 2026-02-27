dcf0b335db44c13a9863a704871e7587e8495b1d
g5n9u1-coder <shashank.singh2_cs.aiml@gla.ac.in>
2026-02-27 17:34:23 +0530

    Define LDR analog pin (Use A0)

--- src/main.cpp
+++ src/main.cpp
@@ -10,35 +10,50 @@
  * and displays structured output via Serial Monitor.
  */
 
- // TODO 1:
- // Define LDR analog pin (Use A0)
+// TODO 1:
+// Define LDR analog pin (Use A0)
+#define LDR_PIN A0
 
- // TODO 2:
- // Create variable to store sensor reading
+// TODO 2:
+// Create variable to store sensor reading
+//int ldrValue = 0;
 
 void setup() {
 
     // TODO 3:
     // Initialize Serial communication (9600 baud rate)
+  //  Serial.begin(9600);
 
     // TODO 4:
     // Print system initialization message
+    //Serial.println("LDR Light Intensity Monitoring System");
+    //Serial.println("Initializing...");
+    delay(2000);
 }
 
 void loop() {
 
     // TODO 5:
     // Read analog value from LDR
+    //ldrValue = analogRead(LDR_PIN);
 
     // TODO 6:
     // Print raw ADC value
+    //Serial.print("Raw ADC Value: ");
+    //Serial.println(ldrValue);
 
     // TODO 7:
     // Apply threshold logic (Bright / Dark detection)
+    if (ldrValue < 500) {
 
-    // TODO 8:
-    // Print brightness status
+        // TODO 8:
+        // Print brightness status
+      //  Serial.println("Bright Environment");
+    } else {
+        //Serial.println("Dark Environment");
+    }
 
     // TODO 9:
     // Add delay (500ms or 1 second)
+    delay(1000);
 }
