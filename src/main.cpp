a5a97d89b7b224487936f6efae941ff731a39a23
g5n9u1-coder <shashank.singh2_cs.aiml@gla.ac.in>
2026-02-27 17:35:00 +0530

    Initialize Serial communication (9600 baud rate)

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,7 @@ void setup() {
 
     // TODO 3:
     // Initialize Serial communication (9600 baud rate)
-  //  Serial.begin(9600);
+    Serial.begin(9600);
 
     // TODO 4:
     // Print system initialization message
